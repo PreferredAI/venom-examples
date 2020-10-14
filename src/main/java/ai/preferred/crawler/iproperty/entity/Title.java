17263dc9db6944fe4dd99faec9f7af9b7dff95e4
Lee Ween Jiann <lwj5@hotmail.com>
2020-10-15 00:15:28 +0800

    Added Title entity

---
+++ src/main/java/ai/preferred/crawler/iproperty/entity/Title.java
@@ -0,0 +1,45 @@
+package ai.preferred.crawler.iproperty.entity;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/**
+ * This class allows you to store your entities. Define the
+ * properties of your entities in this class.
+ */
+public class Title {
+
+  private String id;
+
+  private String title;
+
+  private String category;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getCategory() {
+    return category;
+  }
+
+  public void setCategory(String category) {
+    this.category = category;
+  }
+
+  @Override
+  public String toString() {
+    return ToStringBuilder.reflectionToString(this);
+  }
+}
